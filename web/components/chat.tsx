9226568b9582653146407d6bb562eaa34f08ed1e
niochi273 <valeraveselovsky2000@gmail.com>
2026-07-10 20:10:39 +0700

    Protected routes

---
+++ web/components/chat.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Field, FieldGroup } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { socket } from "@/lib/socket";
+import { Message } from "@/lib/types";
+import { Send } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+
+export default function Chat() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [text, setText] = useState<string>("");
+  const [currentRoom, setCurrentRoom] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    socket.connect();
+
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      console.log("Connected");
+    }
+
+    function onDisconnect() {
+      console.log("Disconected");
+    }
+
+    function onReceive(message: Message) {
+      if (message) {
+        setMessages((messages) => [...messages, message]);
+        window.scrollTo(0, document.body.scrollHeight);
+      }
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    socket.on("chat_message", onReceive);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("chat_message", onReceive);
+      socket.disconnect();
+    };
+  }, []);
+
+  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!text) return;
+
+    const optimisticMessage: Message = {
+      id: crypto.randomUUID(),
+      room: currentRoom,
+      content: text,
+      createdAt: new Date().toISOString(),
+    };
+
+    setMessages((prev) => [...prev, optimisticMessage]);
+    setText("");
+
+    socket.emit("chat_message", text, ({ ok }: { ok: boolean }) => {
+      if (!ok) {
+        setMessages((prev) =>
+          prev.filter((m) => m.id !== optimisticMessage.id),
+        );
+        alert("Message wasn't sent");
+      }
+    });
+  }
+
+  function joinRoom() {
+    socket.emit(
+      "join_room",
+      inputRef.current?.value,
+      ({ room, history }: { room: string; history: Message[] }) => {
+        if (room) {
+          setCurrentRoom(room);
+          setMessages(history);
+        }
+      },
+    );
+  }
+
+  return (
+    <>
+      <header className="p-4">
+        <h1> {currentRoom ?? "No room yet"}</h1>
+        <FieldGroup className="flex flex-row gap-2 ">
+          <Field>
+            <Input ref={inputRef} placeholder="Type..." />
+          </Field>
+          <Button onClick={joinRoom} className="rounded-3xl">
+            Join room
+          </Button>
+        </FieldGroup>
+      </header>
+      <main>
+        <ul>
+          {messages.map((msg) => (
+            <li className="p-2" key={msg.id}>
+              {msg.content}
+            </li>
+          ))}
+        </ul>
+      </main>
+      <footer className="mt-auto p-4">
+        <form onSubmit={handleSubmit}>
+          <FieldGroup className="flex flex-row gap-2 ">
+            <Field>
+              <Input
+                name="message"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Type..."
+              />
+            </Field>
+            <Button type="submit" className="rounded-3xl">
+              <Send />
+            </Button>
+          </FieldGroup>
+        </form>
+      </footer>
+    </>
+  );
+}
---
+++ web/lib/types.ts
@@ -0,0 +1,6 @@
+export type Message = {
+  id: string;
+  content: string;
+  createdAt: string;
+  room: string;
+};
