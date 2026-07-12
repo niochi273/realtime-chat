589c12c76cdcee0e65f65bd30493d939d5be97f4
niochi273 <valeraveselovsky2000@gmail.com>
2026-07-12 18:32:17 +0700

    Add socket authentication and link messages to users

--- server/server.ts
+++ server/server.ts
@@ -3,8 +3,9 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import { db } from "./src/db.js";
-import { messages } from "@repo/shared";
+import { messages, session } from "@repo/shared";
 import { asc, eq } from "drizzle-orm";
+import { parse } from "cookie";
 
 dotenv.config();
 
@@ -15,7 +16,36 @@ const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
+    credentials: true,
   },
+});
+
+io.use(async (socket, next) => {
+  const cookie = socket.handshake.headers.cookie;
+  if (!cookie) return next(new Error("Unauthorized"));
+
+  const parsedCookie =
+    parse(cookie)["better-auth.session_token"]?.split(".")[0];
+
+  if (!parsedCookie) return next(new Error("Unauthorized"));
+
+  try {
+    const dbSession = await db
+      .select()
+      .from(session)
+      .where(eq(session.token, parsedCookie));
+
+    if (dbSession.length === 0) return next(new Error("Unauthorized"));
+
+    if (dbSession[0].expiresAt < new Date()) {
+      return next(new Error("Unauthorized"));
+    }
+
+    socket.data.userId = dbSession[0].userId;
+    next();
+  } catch (e: any) {
+    next(new Error(e.message));
+  }
 });
 
 io.on("connection", (socket) => {
@@ -44,6 +74,7 @@ io.on("connection", (socket) => {
         .values({
           room: currentRoom,
           content: msg,
+          senderId: socket.data.userId,
         })
         .returning();
 
--- web/lib/socket.ts
+++ web/lib/socket.ts
@@ -4,4 +4,5 @@ import { io } from "socket.io-client";
 
 export const socket = io("http://localhost:3001", {
   autoConnect: false,
+  withCredentials: true,
 });
